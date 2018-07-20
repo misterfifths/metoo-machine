414a646b1396891681e9fae3fdc28f1aa023b023
Tim Clem <tim.clem@gmail.com>
2018-07-19 21:24:15 -0400

    Retry twitter stuff on errors

--- esp32/main/twitter_task.c
+++ esp32/main/twitter_task.c
@@ -21,6 +21,14 @@ const uint32_t twitter_task_stack_size = 4 * 1024;
 
 static const char *TAG = "TWT";
 
+static uint32_t retry_delay_ms = 0;
+
+typedef enum {
+	twitter_error_networking,  // TODO: this catches TCP/IP stuff as well as connectivity issues
+	twitter_error_general_http,  // 400 & 500 HTTP statuses
+	twitter_error_http_420  // 420 status, Twitter's (very adult) rate limit
+} twitter_error;
+
 
 static void handle_tweet(cJSON *json)
 {
@@ -28,7 +36,7 @@ static void handle_tweet(cJSON *json)
 }
 
 
-static void read_loop(esp_http_client_handle_t http_client)
+static twitter_error read_loop(esp_http_client_handle_t http_client)
 {
 	const size_t resp_buffer_length = 20 * 1024;
 	char *resp_buffer = calloc(resp_buffer_length, sizeof(char));
@@ -100,12 +108,17 @@ static void read_loop(esp_http_client_handle_t http_client)
 
 cleanup:
 	free(resp_buffer);
+
+	// Making the safe-seeming assumption that trouble at this point is a networking error of some sort
+	return twitter_error_networking;
 }
 
 
-static void connect_to_twitter(void)
+static twitter_error connect_to_twitter(void)
 {
 	const char *url = "https://stream.twitter.com/1.1/statuses/filter.json";
+
+	twitter_error res = twitter_error_networking;
 
 	int params_count = 0;
 	char **params = NULL;
@@ -153,9 +166,9 @@ static void connect_to_twitter(void)
 	ESP_LOGI(TAG, "Opening HTTP connection...");
 
 	esp_err_t err;
-
 	err = esp_http_client_open(http_client, postargs_length);
 	if(err != ESP_OK) {
+		res = twitter_error_networking;
 		ESP_LOGE(TAG, "Error opening connection: %s", esp_err_to_name(err));
 		goto cleanup;
 	}
@@ -163,6 +176,7 @@ static void connect_to_twitter(void)
 
 	int bytes_written = esp_http_client_write(http_client, postargs, postargs_length);
 	if(bytes_written == -1) {
+		res = twitter_error_networking;
 		ESP_LOGE(TAG, "Error writing POST body");
 		goto cleanup;
 	}
@@ -173,6 +187,7 @@ static void connect_to_twitter(void)
 
 	int content_length = esp_http_client_fetch_headers(http_client);
 	if(content_length == -1) {
+		res = twitter_error_networking;
 		ESP_LOGE(TAG, "Error reading response headers");
 		goto cleanup;
 	}
@@ -180,16 +195,19 @@ static void connect_to_twitter(void)
 
 	int status_code = esp_http_client_get_status_code(http_client);
 	if(status_code >= 400) {
-		audio_task_enqueue_sound(audio_task_sound_error);
+		res = status_code == 420 ? twitter_error_http_420 : twitter_error_general_http;
 		ESP_LOGE(TAG, "HTTP response status code %d", status_code);
 		goto cleanup;
 	}
+
+	// Wipe any retry backoff from previous errors
+	retry_delay_ms = 0;
 
 	audio_task_enqueue_sound(audio_task_sound_success3);
 	ESP_LOGI(TAG, "HTTP response status code %d. Entering read loop", status_code);
 
 
-	read_loop(http_client);
+	res = read_loop(http_client);
 
 
 cleanup:
@@ -197,6 +215,8 @@ cleanup:
 
 	esp_http_client_close(http_client);
 	esp_http_client_cleanup(http_client);
+
+	return res;
 }
 
 
@@ -205,13 +225,42 @@ void twitter_task_main(void *task_params)
 	// The ESP HTTP client log level is debug by default, and it is *chatty*
 	esp_log_level_set("HTTP_CLIENT", ESP_LOG_INFO);
 
-	connect_to_twitter();
-	// TODO: reconnect on error
-
-	ESP_LOGI(TAG, "Spinning indefinitely...");
-	fflush(stdout);
 
 	while(1) {
-		vTaskDelay(1000 / portTICK_PERIOD_MS);
+		twitter_error err = connect_to_twitter();
+
+
+		// If connect_to_twitter returns, it means we hit an error.
+		audio_task_enqueue_sound(audio_task_sound_error);
+
+
+		// We'll be backing off on retries as the API docs suggest:
+		// https://developer.twitter.com/en/docs/tweets/filter-realtime/guides/connecting
+		if(err == twitter_error_networking) {
+			// "Back off linearly for TCP/IP level network errors. These problems are generally temporary
+			// and tend to clear quickly. Increase the delay in reconnects by 250ms each attempt, up to 16 seconds"
+			retry_delay_ms += 250;
+
+			if(retry_delay_ms > 16 * 1000) retry_delay_ms = 16 * 1000;
+		}
+		else if(err == twitter_error_general_http) {
+			// "Back off exponentially for HTTP errors for which reconnecting would be appropriate. Start
+			// with a 5 second wait, doubling each attempt, up to 320 seconds."
+			if(retry_delay_ms == 0) retry_delay_ms = 5 * 1000;
+			else retry_delay_ms *= 2;
+
+			if(retry_delay_ms > 320 * 1000) retry_delay_ms = 320 * 1000;
+		}
+		else if(err == twitter_error_http_420) {
+			// "Back off exponentially for HTTP 420 errors. Start with a 1 minute wait and double each
+			// attempt. Note that every HTTP 420 received increases the time you must wait until rate
+			// limiting will no longer will be in effect for your account."
+			// We shouldn't see any of these, but you never know.
+			if(retry_delay_ms == 0) retry_delay_ms = 60 * 1000;
+			else retry_delay_ms *= 2;
+		}
+
+		ESP_LOGI(TAG, "Twitter error. Retrying in %u ms", retry_delay_ms);
+		vTaskDelay(retry_delay_ms / portTICK_PERIOD_MS);
 	}
 }
