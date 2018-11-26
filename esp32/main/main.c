ab60711db80ad0a645705aac8650e496f7cf9b29
Tim Clem <tim.clem@gmail.com>
2018-11-26 12:21:52 -0500

    Wrap task creation in a function that catches errors

--- esp32/main/main.c
+++ esp32/main/main.c
@@ -41,6 +41,20 @@ static void init_networking(void)
 }
 
 
+static TaskHandle_t create_task(TaskFunction_t task_main, const char * const name, uint32_t stack_size, UBaseType_t priority)
+{
+	TaskHandle_t handle = NULL;
+	BaseType_t res = xTaskCreate(task_main, name, stack_size, NULL, priority, &handle);
+
+	if(res != pdPASS) {
+		ESP_LOGE(TAG, "Error creating task '%s': %d", name, res);
+		abort();
+	}
+
+	return handle;
+}
+
+
 void app_main()
 {
 	ESP_LOGI(TAG, "Hello, world!");
@@ -48,15 +62,15 @@ void app_main()
 		ESP_LOGI(TAG, "This is a wake from deep sleep.");
 	}
 
-	xTaskCreate(audio_task_main, "audio_task", audio_task_stack_size, NULL, 4, NULL);
+	create_task(audio_task_main, "audio_task", audio_task_stack_size, 4);
 
-	xTaskCreate(sleep_task_main, "sleep_task", sleep_task_stack_size, NULL, 10, NULL);
+	create_task(sleep_task_main, "sleep_task", sleep_task_stack_size, 10);
 
 	init_networking();
 
 	// You're supposed to wait until wifi starts before attempting to read from ADC1, which
 	// is what the battery voltage is on.
-	xTaskCreate(battery_task_main, "battery_task", battery_task_stack_size, NULL, 3, NULL);
+	create_task(battery_task_main, "battery_task", battery_task_stack_size, 3);
 
-	xTaskCreate(twitter_task_main, "twitter_task", twitter_task_stack_size, NULL, 5, &twitter_task_handle);
+	twitter_task_handle = create_task(twitter_task_main, "twitter_task", twitter_task_stack_size, 5);
 }
