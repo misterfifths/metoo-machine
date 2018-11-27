dedeff8595b2af64c1df9f043472a2d6e5845363
Tim Clem <tim.clem@gmail.com>
2018-11-26 23:13:23 -0500

    Work around stuttering audio problem. Remove old tweet sound.

--- esp32/main/audio_output.c
+++ esp32/main/audio_output.c
@@ -4,10 +4,14 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/queue.h"
+
 #include "esp_log.h"
+
 #include "driver/gpio.h"
 #include "driver/i2s.h"
+
 #include "audio_output.h"
+#include "sound_data.h"
 
 
 static const char *TAG = "AUDIO_OUT";
@@ -34,6 +38,11 @@ static const char *TAG = "AUDIO_OUT";
 #define CONFIG_I2S_CHANNEL_COUNT I2S_CHANNEL_MONO
 
 
+// See note in audio_init below about these weirdnesses.
+static size_t silence_samples_length = 0;
+static unsigned char *silence_samples = NULL;
+
+
 void audio_init()
 {
 	// the i2s module is chatty about DMA buffers
@@ -50,7 +59,7 @@ void audio_init()
 		.communication_format = I2S_COMM_FORMAT_I2S_MSB,  // despite the audio being technically PCM, this is the format you want, apparently
 		.intr_alloc_flags = 0,
 		.dma_buf_count = 2, // 2 is the minimum
-		.dma_buf_len = 64,  // seems this needs to be significantly smaller than the audio sample you're playing, or things get stuttery
+		.dma_buf_len = 64,  // see note below
     };
 
 
@@ -62,18 +71,53 @@ void audio_init()
 
     // Why do you need to specify sample rate and bits/sample in i2s_driver_install and also with this?
     ESP_ERROR_CHECK(i2s_set_clk(CONFIG_I2S_NUM, CONFIG_I2S_SAMPLE_RATE, CONFIG_I2S_BITS_PER_SAMPLE, CONFIG_I2S_CHANNEL_COUNT));
+
+
+    /*
+	* Re: i2s_config_t.dma_buf_len and noise: here's what I've gleaned.
+	* If your audio samples sent via i2s_write don't have a certain amount of silence at the end, then
+	* you'll get a nasty droning sound after your sound finishes.
+	* This seems to be an underrun of the DMA buffers (see, e.g., https://github.com/earlephilhower/ESP8266Audio/issues/48).
+	* Calling i2s_zero_dma_buffer before i2s_write seems like it would be the ideal solution,
+	* but it causes popping sounds (probably because "silence" isn't just zero bytes).
+	*
+	* If we work on the suspicion that we need to fill the DMA buffers with (actual) silence after a
+	* sound, we can find the formula for the size of the DMA buffers in i2s.c, function i2s_create_dma_queue():
+	* 	dma_buf_count * dma_buf_len * bytes_per_sample * channel_count
+	*
+	* So to work around, here we calculate that size and make a buffer full of that many bytes of silence.
+	* The script that generates sound_data.{c,h} also generates one sample of silence in the same format as
+	* the rest of the audio. We expand that to a longer buffer below, and play_sound() writes that buffer
+	* to i2s after every sound.
+	*/
+
+    const int channel_count = i2s_config.channel_format < I2S_CHANNEL_FMT_ONLY_RIGHT ? 2 : 1;
+    silence_samples_length = i2s_config.dma_buf_count * i2s_config.dma_buf_len * (CONFIG_I2S_BITS_PER_SAMPLE / 8) * channel_count;
+    ESP_LOGI(TAG, "Will be appending %zu bytes of silence to each sound", silence_samples_length);
+
+    silence_samples = malloc(silence_samples_length);
+
+    size_t template_i = 0;
+    for(size_t i = 0; i < silence_samples_length; i++) {
+    	silence_samples[i] = sound_silence_sample[template_i];
+
+    	template_i = (template_i + 1) % sound_silence_sample_len;
+    }
 }
 
 
 void play_sound(const unsigned char *samples, size_t samples_length, bool sync)
 {
 	const int mono_divisor = CONFIG_I2S_CHANNEL_COUNT == I2S_CHANNEL_MONO ? 2 : 1;
-	const uint32_t sound_length_ms = ((double)samples_length / CONFIG_I2S_SAMPLE_RATE / mono_divisor) * 1000;
-	ESP_LOGD(TAG, "Playing %zu samples, for a length of %u ms", samples_length, sound_length_ms);
+	const size_t total_samples_length = samples_length + silence_samples_length;
+	const uint32_t sound_length_ms = ((double)total_samples_length / CONFIG_I2S_SAMPLE_RATE / mono_divisor) * 1000;
+	ESP_LOGD(TAG, "Playing %zu samples, for a length of %u ms", total_samples_length, sound_length_ms);
 
 	size_t bytes_written;
 	ESP_ERROR_CHECK(i2s_write(CONFIG_I2S_NUM, samples, samples_length, &bytes_written, portMAX_DELAY));
-//	ESP_LOGD(TAG, "Wrote %zu bytes of audio (%zu total)", bytes_written, samples_length);
+
+	// See big fat note in audio_init() about the purpose and duration of this silence.
+	ESP_ERROR_CHECK(i2s_write(CONFIG_I2S_NUM, silence_samples, silence_samples_length, &bytes_written, portMAX_DELAY));
 
 	if(sync) {
 		vTaskDelay(sound_length_ms / portTICK_PERIOD_MS);
