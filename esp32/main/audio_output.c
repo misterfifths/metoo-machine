5e52e5c1abd8157ba8bc787cb2dabf06bf5974b9
Tim Clem <tim.clem@gmail.com>
2018-11-26 23:26:55 -0500

    Remove some unneeded includes

--- esp32/main/audio_output.c
+++ esp32/main/audio_output.c
@@ -3,7 +3,6 @@
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "freertos/queue.h"
 
 #include "esp_log.h"
 
--- esp32/main/sleep_task.c
+++ esp32/main/sleep_task.c
@@ -3,7 +3,6 @@
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "freertos/semphr.h"
 
 #include "esp_log.h"
 #include "esp_sleep.h"
