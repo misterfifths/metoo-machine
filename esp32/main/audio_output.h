8253b25d995de7c05d7f4623ec74e5c8f8e5eccf
Tim Clem <tim.clem@gmail.com>
2018-12-04 15:04:38 -0500

    Add note about audio_task to audio_output.h

--- esp32/main/audio_output.h
+++ esp32/main/audio_output.h
@@ -12,6 +12,8 @@ void audio_init(void);
 
 // If sync is true, this call will suspend the calling task (waiting on an internal queue)
 // until the sound is finished playing.
+// You probably want to go through the interface in audio_task.h, which lets you enqueue
+// sounds in a fire-and-forget manner.
 void play_sound(const unsigned char *samples, size_t samples_length, bool sync);
 
 
